34d3939a2b9dbc60e1d650bc525672194940a900
mzambia70 <mzambia70@gmail.com>
2019-05-01 08:55:45 +0300

    adding buttons to my work which is in quote-details

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,13 +1,34 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit, Input , Output, EventEmitter} from '@angular/core';
 import {Quote} from '../quote'
-
+import {TimeAgoPipe} from 'time-ago-pipe';
 @Component({
   selector: 'app-quote-details',
   templateUrl: './quote-details.component.html',
   styleUrls: ['./quote-details.component.css']
 })
 export class QuoteDetailsComponent implements OnInit {
-    @Input()quote:Quote;
+
+
+  @Input() quote:Quote;
+  @Output() isComplete =new EventEmitter <boolean>();
+
+  uvote: number=0;
+  dvote: number= 0;
+
+  toogleDetails(index){
+    this.quote[index].showDescription= !this.quote[index].showDescription;
+  }
+
+  upVote(){
+    this.uvote+=1;
+  }
+  downVote(){
+    this.dvote +=1;
+  }
+
+  deleteQuote(complete:boolean){
+    this.isComplete.emit(complete);
+  }
 
   constructor() { }
 
