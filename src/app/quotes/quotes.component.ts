fc1ae891b3351462a1276fb2c789c94e7f3a0a9f
mzambia70 <mzambia70@gmail.com>
2019-05-01 11:16:09 +0300

    updating my quote-form with changes in the file format

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Quote } from '../quote'
 @Component({
-  selector: 'app-quote',
-  templateUrl: './quote.component.html',
-  styleUrls: ['./quote.component.css']
+  selector: 'app-quotes',
+  templateUrl: './quotes.component.html',
+  styleUrls: ['./quotes.component.css']
 })
 export class QuoteComponent implements OnInit {
   quotes = [
