b125ebe67a5f9a2980558b2be9d134009550b59e
mzambia70 <mzambia70@gmail.com>
2019-05-01 11:19:38 +0300

    updating my quote-details with changes

--- src/app/quote-details/quote-details.component.spec.ts
+++ src/app/quote-details/quote-details.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { QuoteDetailsComponent } from './quote-details.component';
 
